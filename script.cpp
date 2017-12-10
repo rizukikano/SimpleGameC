919fcb68f125a7ea86729cd909752418fd1c9326
RizukiKano <rizki_avenzho@yahoo.com>
2017-12-10 15:46:14 +0700

    NN

--- script.cpp
+++ script.cpp
@@ -1,12 +1,18 @@
-#include<iostream>
-#include<string.h>
-
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+ 
 using namespace std;
-
+ 
+bool randBool();
+void createWord(string, string);
+void printWord(string);
+ 
+string answer;
+ 
 int main()
-{	
-	string answer;
-	
+{
   string words[] = {
     "DOTA 2",
     "DEVIL MY CRY",
@@ -14,34 +20,65 @@ int main()
     "ICEFROG",
     "ASSASSIN CREED"
   };
-
-  string hint[] = {
-    "Sebuah permainan multiplayer online battle arena, \n merupakan sekuel dari Defense of the Ancients mod pada Warcraft 3: Reign of Chaos \n dan Warcraft 3: The Frozen Throne",
+ 
+  string hints[] = {
+    "Sebuah permainan multiplayer online battle arena, merupakan sekuel dari Defense of the Ancients mod pada Warcraft 3: Reign of Chaos dan Warcraft 3: The Frozen Throne",
     "Game series yang dikembangkan CAPCOM",
     "Game yang booming dengan teknologi AR nya ",
     "Pencetus DOTA 2",
     "Game yang dikembangkan oleh UBISOFT "
   };
-	
-	for (int i = 0; i < words[0].length(); i++)
-	{	
-		if (i % 2 != 0)
-		{
-			cout << "_";
-		} else {
-			cout << words[0][i];
-		}
-		
-		cout << " ";
-	}
-	
-	cout << endl << endl << "Kata yang benar = ";
-	getline(cin, answer);
-	
-	if (answer == words[0])
-	{
-		cout << "Anda Benar";
-	} else {
-		cout << "Anda Salah";
-	}
+ 
+  int size = sizeof(words) / sizeof(words[0]);
+  srand(time(NULL));
+ 
+  for (int i = 0; i < size; i++) {
+    createWord(words[i], hints[i]);
+  }
+}
+ 
+bool randBool()
+{
+  return (rand() % 2 == 0) ? true : false;
+}
+ 
+void createWord(string word, string hint)
+{
+  int nullCount = 0, wordCount = 0, limit;
+  string temp = word, replace;
+ 
+  wordCount = word.length();
+  limit = wordCount / 3;
+  nullCount = 0;
+ 
+  while (nullCount < limit) {
+    for (int j = 0; j < wordCount; j++)
+    {
+      if (randBool() && nullCount < limit && temp[j] != ' ' && temp[j] != '_')
+      {
+        temp.replace(j, 1, "_");
+        nullCount++;
+      }
+    }
+  }
+ 
+  printWord(temp);
+ 
+  cout << endl << endl << "Hint : " << hint << endl;
+  cout << endl << endl << "Kata yang benar = ";
+  getline(cin, answer);
+ 
+  if (answer == word)
+  {
+    cout << "Anda Benar" << endl << endl;
+  } else {
+    cout << "Anda Salah" << endl << endl;
+  }
+}
+ 
+void printWord(string word)
+{
+  for (int i = 0; i < word.length(); i++) {
+    cout << word[i] << " ";
+  }
 }
